4dfed4fad0d61fa08e14d408b95457451e69f460
Neyaz-office <mohammed.neyaz@brandsystems.com>
2025-07-22 16:41:47 +0530

    this is a sub branch

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -22,6 +22,7 @@ function MyComponent() {
             <input type="number" value={car.year} onChange={handleYearChange}></input><br/>
             <input type="text" value={car.maker} onChange={handleMakerchange}></input><br/>
             <input type="text" value={car.Model} onChange={handleModelChange}></input>
+             <input type="text" value={car.Model} onChange={handleModelChange}></input>
         </div>
     );
 }
