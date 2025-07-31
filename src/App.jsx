4750cb1f8ce6f7ebdc9b0a65ac9d3cf2ef6925da
Neyaz-office <mohammed.neyaz@brandsystems.com>
2025-07-31 15:18:46 +0530

    learnt to optamize the useState()

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import CommentSection from "./CommentSection.jsx";
-
+//import Aswinilearn from "./LearnUseRef.jsx";
+//import ExpensiveCalculationComponent from "./ExpensiveCalculationComponents.jsx"
+import MyComponents from "./MyComponents.jsx";
 //import ToDoList from "./ToDoList.jsx";
 
 
 function App() {
    return(
     <>
-        <CommentSection/>    
+        <MyComponents/>  
     </>
    );
 }
