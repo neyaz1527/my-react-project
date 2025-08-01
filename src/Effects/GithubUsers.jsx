79e8e634987aa42f482055e7de333dd99dfbfd22
Neyaz-office <mohammed.neyaz@brandsystems.com>
2025-08-01 15:45:26 +0530

    done with the git user sample project

--- src/Effects/GithubUsers.jsx
+++ src/Effects/GithubUsers.jsx
@@ -1,36 +1,49 @@
-import React, {useState, useEffect} from "react";
-import './UserEffect.css'
+import React, { useState, useEffect } from "react";
+import "./UserEffect.css";
 
 function GithubUsers() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const [users, setUsers] = useState([]);
-
-    async function fetchUsers(){
-        try{
-            const response = await fetch('https://api.github.com/users')
-            const result = await response.json();
-            setUsers(result);
-        }
-        catch(error) {}
+  async function fetchUsers() {
+    try {
+      const response = await fetch("https://api.github.com/users");
+      const result = await response.json();
+      setUsers(result);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
     }
+  }
 
-    fetchUsers();
-    return (
-        <div className="users">
-            <h1>Github users</h1>
-            <ul>
-                {users.map((user) =>{
-                    const { id, avatar_url, login, html_url} = users;
+  useEffect(() =>{
+    setTimeout(() => {
+      fetchUsers();
+    }, 1000);
+  }, []);
 
-                    return(
-                        <li key={id}>
-                            <img src={avatar_url} alt={login}/>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-    );
+  if(loading){
+    return <p>Loading....</p>
+  }
+  if(!loading){
+  return (
+    <div className="users">
+      <h1>Github users</h1>
+      <ul>
+        {users.map((user) => {
+          const { id, avatar_url, login, html_url } = user;
+
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <p>{login}</p>
+              <a href={html_url} target="_blank" className="profile-btn">Profile</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );}
 }
 
 export default GithubUsers;
