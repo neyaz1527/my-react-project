5d61ca16b9dc10b4063a8e5bafa62a50bbd85d37
neyaz <zayen2001@gmail.com>
2025-08-03 00:40:07 +0530

    done with the add to cart project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 //import GithubUsers from "./Effects/GithubUsers.jsx";
-//import Home from './home/Home.jsx';
+import Home from './home/Home.jsx';
 //import Counter from './components/state/Counter.jsx'
-import Cart from './components/state/Cart.jsx'
+//import Cart from './components/state/Cart.jsx'
 function App() {
    return(
     <div  className="container">
-        <Cart/>  
+        <Home/>  
     </div>
    );
 }
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import Cart from "../components/state/Cart.jsx";
+import "./Home.css";
 
 export default function Home() {
+  const [productName, setProductName] = useState("");
+  const [price, setPrice] = useState("");
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (e) => {
+    e.preventDefault();
+    if (productName && price) {
+      const newProduct = {
+        id: Date.now(),
+        name: productName,
+        price: parseFloat(price),
+      };
+      setCartItems([...cartItems, newProduct]);
+      setPrice("");
+      setProductName("");
+    }
+  };
+  
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1>Add product</h1>
+      <form className="form" onSubmit={handleAddToCart}>
+        <label htmlFor="productName">Product Name</label>
+        <input
+          type="text"
+          value={productName}
+          onChange={(e) => {
+            setProductName(e.target.value);
+          }}
+        ></input>
+        <label htmlFor="price">Price</label>
+        <input
+          type="number"
+          value={price}
+          onChange={(e) => {
+            setPrice(e.target.value);
+          }}
+        ></input>
+        <button className="btn-submit" type="submit">Add to cart</button>
+      </form>
+      <Cart products={cartItems} setProducts={setCartItems} />
+    </div>
+  );
 }
