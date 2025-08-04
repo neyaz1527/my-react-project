5cecf68149f8243697c92a65817f91709be4cdbd
Neyaz-office <mohammed.neyaz@brandsystems.com>
2025-08-04 15:03:29 +0530

    add a node componenet in the login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-//import GithubUsers from "./Effects/GithubUsers.jsx";
+import GithubUsers from "./Effects/GithubUsers.jsx";
 import Home from './home/Home.jsx';
 //import Counter from './components/state/Counter.jsx'
 //import Cart from './components/state/Cart.jsx'
 import LoginForm from './ref/LoginForm.jsx'
+import LearnCallback from './LearnCallback.jsx';
 function App() {
    return(
     <div  className="container">
